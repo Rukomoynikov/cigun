94fabaa794122e9a2a52be563b4216c908ddd12b
Maksim Rukomoynikov <rukomoynikov@Maksims-Mac-mini.local>
2015-08-26 17:31:47 +0400

    Starting Wordpress Development

---
+++ wordpress-theme/footer.php
@@ -0,0 +1,18 @@
+<div id="register_block" class="register_form">
+      <form id="register_form">
+        <h3>Регистрация на событие</h3>
+        <div class="formgroup">
+          <label for="fio">Ваше имя: </label>
+          <input type="text" name="fio" required>
+        </div>
+        <div class="formgroup">
+          <label for="email">Ваше имя: </label>
+          <input type="email" name="email" required>
+        </div>
+        <div class="formgroup submit">
+          <input type="submit">
+        </div>
+      </form>
+    </div>
+  </body>
+</html>
---
+++ wordpress-theme/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/css/animate.css" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/fonts/fontawesome/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/fonts/roboto/robotonew.css" type="text/css">
+    <meta charset="utf-8">
+    <title>Семинар по древним китайским оздоровительным практикам</title>
+    <!-- Tags for Facebook-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="">
+    <meta property="og:site_name" content="">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="http://30621.selcdn.ru/justtest/cigun/img/jumbo_back.jpg">
+    <!-- scripts-->
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/jquery.min.js" type="text/javascript"></script>
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/wow.min.js"></script>
+    <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/gmaps.js"></script>
+    <!--fancybox-->
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/assets/js/fancybox/jquery.fancybox.css" type="text/css">
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/fancybox/jquery.fancybox.js"></script>
+    <!-- app.js-->
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/assets/js/app.js"></script>
+  </head>
+  <body>
